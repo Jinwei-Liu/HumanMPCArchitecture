a8d3c05ccbb9cf54a086cdff00bb515b80a04229
Jinwei_Liu <liujinweishandong@outlook.com>
2025-07-11 14:18:12 +0800

    debug

--- Personalized_SA/config/config.py
+++ Personalized_SA/config/config.py
@@ -7,7 +7,7 @@ import numpy as np
 parser = argparse.ArgumentParser()
 parser.add_argument("--episodes", type=int, default=3000)
 parser.add_argument("--log_dir", type=str, default="./Personalized_SA/human_model/runs_quad")
-parser.add_argument("--max_steps", type=int, default=2000)
+parser.add_argument("--max_steps", type=int, default=5000)
 parser.add_argument("--hid_shape", nargs=3, type=int, default=[128, 128, 128])
 parser.add_argument("--actor_lr", type=float, default=1e-3)
 parser.add_argument("--critic_lr", type=float, default=1e-3)
